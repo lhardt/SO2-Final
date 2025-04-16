c02918d8d79ad8ebfb99f740b7d8f1d5c93795d5
lhardt <22939160+lhardt@users.noreply.github.com>
2025-04-16 09:09:34 -0300

    Multithread Client (#5)

---
+++ src/client.hpp
@@ -0,0 +1,28 @@
+#include <thread>
+#include <chrono>
+#include <string>
+
+typedef class Client Client;
+
+void g_handleIoThread(Client * client);
+void g_handleFileThread(Client * client);
+void g_handleNetworkThread(Client * client);
+
+class Client {
+public:
+    Client(std::string client_name, std::string server_ip, std::string server_port);
+
+    void handleIoThread();
+    void handleFileThread();
+    void handleNetworkThread();
+
+    std::thread io_thread;
+    std::thread file_thread;
+    std::thread network_thread;
+
+    std::string client_name;
+    std::string server_ip;
+    std::string server_port;
+
+    // queue of commands and lock?
+};
