49694c10aa505656c26c6dfeaf73622e531c5362
jo2099 <fleckjoaocarlos@gmail.com>
2025-05-12 18:55:42 -0300

    Inotify working

--- src/Client/client.cpp
+++ src/Client/client.cpp
@@ -102,57 +102,55 @@ void Client::handleFileThread() {
   log_info("File thread | Connected to server: %s:%d", this->server_ip.c_str(),
            this->file_watcher_port);
 
-  return;
-}
+  int inotifyFd = inotify_init1(IN_NONBLOCK);
+  if (inotifyFd < 0) {
+    log_error("Could not initiate inotify");
+    return;
+  }
+  int watchDescriptor =
+      inotify_add_watch(inotifyFd, sync_dir, IN_DELETE | IN_CLOSE_WRITE);
+  if (watchDescriptor < 0) {
+    log_error("Could not initiate watchDescriptor");
+    return;
+  }
 
-// int inotifyFd = inotify_init1(IN_NONBLOCK);
-// if (inotifyFd < 0) {
-//   log_error("Could not initiate inotify");
-//   return;
-// }
-// int watchDescriptor =
-//     inotify_add_watch(inotifyFd, sync_dir, IN_DELETE | IN_CLOSE_WRITE);
-// if (watchDescriptor < 0) {
-//   log_error("Could not initiate watchDescriptor");
-//   return;
-// }
-//
-// size_t BUF_LEN = 1024 * (sizeof(inotify_event) + 16);
-// char buffer[BUF_LEN];
-//
-// log_info("Monitoring file: %s", sync_dir);
-//
-// while (true) {
-//   int length = read(inotifyFd, buffer, BUF_LEN);
-//   if (length < 0) {
-//     std::chrono::milliseconds sleep_time{500};
-//     std::this_thread::sleep_for(sleep_time); // Evita busy waiting
-//     continue;
-//   }
-//
-//   int i = 0;
-//   while (i < length) {
-//     struct inotify_event *event = (struct inotify_event *)&buffer[i];
-//
-//     if (event->len > 0) {
-//       std::string filepath =
-//           "./" + std::string(sync_dir) + "/" + std::string(event->name);
-//
-//       if (event->mask & IN_CLOSE_WRITE) {
-//         log_info("Arquivo modificado: %s", filepath.c_str());
-//         // FAZER FUNÇÃO QUE ENVIA ARQUIVO PARA SERVIDOR EM UMA
-//         connections.cpp
-//         // (ou algo assim) uploadFile(sock, filepath)
-//       } else if (event->mask & IN_DELETE) {
-//         log_info("Arquivo removido: %s", filepath.c_str());
-//         // FAZER FUNÇÃO QUE ENVIA NOME DO ARQUIVO A SER DELETADO EM UMA
-//         // connections.cpp (ou algo assim) deleteFile(sock, event->name)
-//       }
-//     }
-//
-//     i += sizeof(struct inotify_event) + event->len;
-//   }
-// }
+  size_t BUF_LEN = 1024 * (sizeof(inotify_event) + 16);
+  char buffer[BUF_LEN];
+
+  log_info("Monitoring file: %s", sync_dir);
+
+  while (true) {
+    int length = read(inotifyFd, buffer, BUF_LEN);
+    if (length < 0) {
+      std::chrono::milliseconds sleep_time{500};
+      std::this_thread::sleep_for(sleep_time); // Evita busy waiting
+      continue;
+    }
+
+    int i = 0;
+    while (i < length) {
+      struct inotify_event *event = (struct inotify_event *)&buffer[i];
+
+      if (event->len > 0) {
+        std::string filepath =
+            "./" + std::string(sync_dir) + "/" + std::string(event->name);
+
+        if (event->mask & IN_CLOSE_WRITE) {
+          log_info("Arquivo modificado: %s", filepath.c_str());
+          // FAZER FUNÇÃO QUE ENVIA ARQUIVO PARA SERVIDOR EM UMA
+          // connections.cpp
+          // (ou algo assim) uploadFile(sock, filepath)
+        } else if (event->mask & IN_DELETE) {
+          log_info("Arquivo removido: %s", filepath.c_str());
+          // FAZER FUNÇÃO QUE ENVIA NOME DO ARQUIVO A SER DELETADO EM UMA
+          // connections.cpp (ou algo assim) deleteFile(sock, event->name)
+        }
+      }
+
+      i += sizeof(struct inotify_event) + event->len;
+    }
+  }
+}
 
 void Client::handleNetworkThread() {
   log_info("Started Network Thread with ID %d ", std::this_thread::get_id());
@@ -251,6 +249,13 @@ Client::Client(std::string _client_name, std::string _server_ip,
   int port2 = std::stoi(port_str2);
   std::cout << "Porta recebida do servidor: " << port2 << std::endl;
   this->push_port = port2;
+
+  std::string sync_dir = "sync_dir";
+  if (mkdir(sync_dir.c_str(), 0777) == -1) {
+    std::cerr << "Erro ao criar o diretório: " << sync_dir << std::endl;
+  } else {
+    std::cout << "Diretório criado: " << sync_dir << std::endl;
+  }
 
   // this->io_thread = std::thread(g_handleIoThread, this);
   // this->network_thread = std::thread(g_handleNetworkThread, this);
